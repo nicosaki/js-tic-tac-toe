9da001f569c52d1cb7ce319001d1c1bbd1cb91b2
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-06-30 16:53:37 -0700

    redefined locations of squares on board, and js boardReplica to match

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,7 @@ function TicTacToe() {
   this.firstRow = this.board.children('#first')
   this.secondRow = this.board.children('#second')
   this.thirdRow = this.board.children('#third')
-  this.boardReplica = [[this.firstRow.children('.left'), this.firstRow.children('.mid'), this.firstRow.children('.right')], [this.secondRow.children('.left'), this.secondRow.children('.mid'), this.secondRow.children('.right')], [this.thirdRow.children('.left'), this.thirdRow.children('.mid'), this.thirdRow.children('.right')]]
+  this.boardReplica = [[this.firstRow.children('.[0] [0]'), this.firstRow.children('.[0] [1]'), this.firstRow.children('.[0] [2]')], [this.secondRow.children('.[1] [0]'), this.secondRow.children('.[1] [1]'), this.secondRow.children('.[1] [2]')], [this.thirdRow.children('.[2] [0]'), this.thirdRow.children('.[2] [1]''), this.thirdRow.children('.[2] [2]')]]
 }
 
 TicTacToe.prototype = {
@@ -36,7 +36,7 @@ TicTacToe.prototype = {
     var gamePiece = x
     this.board.on('click', this.playTurn(gamePiece))
   }
-  
+
   this.won = function () {
     for (i = 0; i < 3; i++) {
       if (this.boardReplica[i][0] === this.boardReplica[i][1] === this.boardReplica[i][2]) {
