930476472240e23563577bcf44856cb0791f7c40
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-07-01 16:39:52 -0700

    Almost works. Dunno wtf is up with the win determination function. Logic seems sound to me :/

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,59 +1,52 @@
 function TicTacToe() {
   this.board = $('table')
-  this.firstRow = this.board.children('#first')
-  this.secondRow = this.board.children('#second')
-  this.thirdRow = this.board.children('#third')
-  this.boardReplica = [[this.firstRow.children('.[0] [0]'), this.firstRow.children('.[0] [1]'), this.firstRow.children('.[0] [2]')], [this.secondRow.children('.[1] [0]'), this.secondRow.children('.[1] [1]'), this.secondRow.children('.[1] [2]')], [this.thirdRow.children('.[2] [0]'), this.thirdRow.children('.[2] [1]''), this.thirdRow.children('.[2] [2]')]]
-}
-
-TicTacToe.prototype = {
-  this.play = function () {
-    while (this.won() === false) {
-      this.playerOne()
-      if (this.won() === false){
-        this.playerTwo()
+  this.won = function (count) {
+      if (count < 5) {
+        return false
+      }
+      for (i = 0; i < 3; i++) {
+        console.log($('#' + i + '-' + i).contents()[0])
+        if (($('#' + i + '-' + 0).contents()[0] === $('#' + i + '-' + 1).contents()[0]) && ($('#' + i + '-' + 0).contents()[0] === $('#' + i + '-' + 2).contents()[0]) && (($('#' + i + '-' + 0).contents() !== []))) {
+          return $('#' + i + '-' + 0).contents()[0]
+        } else if (($('#' + 0 + '-' + i).contents()[0] === $('#' + 1 + '-' + i).contents()[0]) && ($('#' + 0 + '-' + i).contents()[0] === $('#' + 2 + '-' + i).contents()[0]) && (($('#' + 0 + '-' + i).contents() !== []) )) {
+        return $('#' + 0 + '-' + i).contents()[0]
       }
     }
-    console.log('Winning player: ' + this.won())
-  }
-
-  this.playTurn = function (gamePiece) {
-    event.preventDefault()
-    var button = $(this)
-    if (button.is(':empty')) {
-      var coordinates = button.class
-      var square = this.board.children('.' + coordinates)[0]
-      square.append(gamePiece)
+    if ($('#' + 0 + '-' + 0).contents()[0] === $('#' + 1 + '-' + 1).contents()[0] && $('#' + 0 + '-' + 0).contents()[0]  === $('#' + 2 + '-' + 2).contents()[0] && $('#' + 0 + '-' + 0).contents() !== []) {
+      return $('#' + 0 + '-' + 0).contents()[0]
+    } else if ($('#' + 2 + '-' + 0).contents()[0] === $('#' + 1 + '-' + 1).contents()[0] && $('#' + 2 + '-' + 0).contents()[0] === $('#' + 2 + '-' + 0).contents()[0] && $('#' + 2 + '-' + 0).contents() !== []){
+      return $('#' + 2 + '-' + 0).contents()[0]
+    } else if (count === 9) {
+      return "Draw"
     }
+    return false
   }
 
-  this.playerOne = function () {
-    var gamePiece = o
-    this.board.on('click', this.playTurn(gamePiece))
+  this.playTurn = function (game) {
+    var plays = 0
+      this.board.on('click', function () {
+        event.preventDefault()
+        if (game.won(plays) === false && plays < 9){
+          var button = event.target
+          var coordinates = button.id
+          if ($('#' + coordinates).contents()[0] !== 'x' && $('#' + coordinates).contents()[0] !== 'o') {
+                if (plays % 2 === 0){
+                  var gamePiece = 'o'
+                  plays++
+                } else {
+                  var gamePiece = 'x'
+                  plays++
+                }
+            $(document.getElementById(coordinates)).text(gamePiece)
+            if (game.won(plays) !== false) {
+              alert("Winner: " + game.won(plays))
+              return false
+            }
+          }
+      } else {
+        alert("Winner: " + game.won(plays))
+      }
+    })
   }
 
-  this.playerTwo = function () {
-    var gamePiece = x
-    this.board.on('click', this.playTurn(gamePiece))
-  }
-
-  this.won = function () {
-    for (i = 0; i < 3; i++) {
-      if (this.boardReplica[i][0] === this.boardReplica[i][1] === this.boardReplica[i][2]) {
-        return this.boardReplica[i][0]
-      } else if (this.boardReplica[0][i] === this.boardReplica[1][i] === this.boardReplica[2][i]) {
-      return this.boardReplica[0][i]
-    } else if (this.boardReplica[0][0] === this.boardReplica[1][1] === this.boardReplica[2][2]) {
-      return this.boardReplica[0][0]
-    } else if (this.boardReplica[2][0] === this.boardReplica[1][1] === this.boardReplica[2][0]){
-      return this.boardReplica[2][0]
-    }
-  }
-  return false
 }
-
-    // if ((this.boardReplica[0][0] === this.boardReplica[0][1]) && (this.boardReplica[0][0] === this.boardReplica[0][2]))
-  }
-}
-
-module.exports = TicTacToe
